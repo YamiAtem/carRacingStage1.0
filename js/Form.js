107f25b96cb6c9d1665dab59a3318b06fd2defb2
Atharva Mishra <atharvamishra956@gmail.com>
2020-11-21 20:04:55 +0530

    made element position cooperate with canvas position

--- js/Form.js
+++ js/Form.js
@@ -14,12 +14,12 @@ class Form {
   display(){
     var title = createElement('h2')
     title.html("Car Racing Game");
-    title.position(130, 0);
+    title.position(displayWidth/2 - 50, 0);
 
-    this.input.position(130, 160);
-    this.button.position(250, 200);
+    this.input.position(displayWidth/2 - 50, 160);
+    this.button.position(displayWidth/2 - 50, 200);
 
-    this.button.mousePressed(()=>{
+    this.button.mousePressed(() => {
       this.input.hide();
       this.button.hide();
       player.name = this.input.value();
@@ -28,7 +28,7 @@ class Form {
       player.update();
       player.updateCount(playerCount);
       this.greeting.html("Hello " + player.name)
-      this.greeting.position(130, 100);
+      this.greeting.position(displayWidth/2 - 50, 100);
     });
 
   }
